743f212a5205fef94bc93caa17594e4f92a56d6c
toplindave <115676942+toplindave@users.noreply.github.com>
2023-09-28 17:46:57 +0100

    Update Spiral.js

--- src/components/spiral/Spiral.js
+++ src/components/spiral/Spiral.js
@@ -4,8 +4,8 @@ import "./spiral.css";
 function Spiral() {
 	return (
 		<div className="spiral">
-			<img src="/asset/spiraal.svg" alt="" />
-			<img src="/asset/spiraaal.svg" alt="" />
+			<img src="/asset/spiraal.svg" alt="top-spiral" />
+			<img src="/asset/spiraaal.svg" alt="bottom-spiral" />
 		</div>
 	);
 }
